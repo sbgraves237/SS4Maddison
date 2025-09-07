9a0061571122e3d2f428a08f0f0ea473314b6bcd
sbgraves237 <spencer.graves@effectivedefense.org>
2025-09-07 13:08:13 -0500

    new MaddisonProjectData.R

---
+++ R/MaddisonProjectData.R
@@ -0,0 +1,24 @@
+#' Maddison Project data 
+#'
+#' The 
+#' \href{https://en.wikipedia.org/wiki/Maddison_Project}{Maddison project} 
+#' collates historical economic statistics from many sources. 
+#'
+#' @format ## `MaddisonData`
+#' A data frame with 4 columns:
+#' \describe{
+#'   \item{countrycode}{3-letter ISO country code}
+#'   \item{year}{Year}
+#'   \item{gdppc}{Real Gross Domestic Product (GDP) per capita in 2011$ at purchasing power parity (PPP)}
+#'   \item{pop}{population}
+#' }
+#' @format ## `MaddisonCountries`
+#' A data frame with 3 columns: 
+#' \describe{
+#'   \item{country}{Country name used by the Maddison project}
+#'   \item{countrycode}{3-letter ISO country code}
+#'   \item{region}{Geographic region including `country`}
+#' }
+#' @source <https://www.rug.nl/ggdc/historicaldevelopment/maddison/releases/maddison-project-database-2020?lang=en>"Groningen Growth and Development Centre"
+
+devtools::load_all() # don't source R/
